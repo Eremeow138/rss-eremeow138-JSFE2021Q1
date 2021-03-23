a6f841ffb772fe4a260843d0cbd1ee6e23688d36
Aleksei Eremin <eremeow138@yandex.ru>
2021-03-23 13:12:24 +0800

    feat: implement reset for filters

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,13 +1,33 @@
 const filters = document.querySelector('.filters');
+const inputs = filters.querySelectorAll('input');
+const btnReset = document.querySelector('.btn-reset');
+const defaultFilters = {};
 
-function handleUpdate(event) {
-  event.target.parentNode.querySelector('output').value = event.target.value; // меняем значение аутпута
-  const suffix = event.target.dataset.sizing || '';
+document.addEventListener('DOMContentLoaded', (e) => {
+  // при загрузке документа записываем в объект defaultFilters значения фильтров
+  inputs.forEach((item) => {
+    defaultFilters[item.name] = item.value;
+  });
+});
+//функция изменения значения переменной css
+function handleUpdate(input) {
+  //input - это определенный инпут на странице.
+  input.parentNode.querySelector('output').value = input.value; // меняем значение аутпута
+  const suffix = input.dataset.sizing || '';
   document.documentElement.style.setProperty(
-    `--${event.target.name}`,
-    event.target.value + suffix
+    `--${input.name}`,
+    input.value + suffix
   );
 }
+//функция изменения значений переменных css на дефолтное
+function handleUpdateDefault(inputs) {
+  inputs.forEach((item) => {
+    const val = defaultFilters[item.name];
+    item.value = val;
+    handleUpdate(item);
+  });
+}
 
-filters.addEventListener('input', event => handleUpdate(event));
+filters.addEventListener('input', (event) => handleUpdate(event.target));
 
+btnReset.addEventListener('click', () => handleUpdateDefault(inputs));
