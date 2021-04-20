50c37829540729fd407354ec6263d5bcee287429
Aleksei Eremin <eremeow138@yandex.ru>
2021-04-20 15:13:47 +0800

    feat: add function getting next image

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,7 +1,12 @@
 const filters = document.querySelector('.filters');
 const inputs = filters.querySelectorAll('input');
 const btnReset = document.querySelector('.btn-reset');
+const btnNext = document.querySelector('.btn-next');
 const defaultFilters = {};
+const image = document.querySelector('.editor img');
+const baseLink =
+  'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/';
+let imageCounter = 1;
 
 document.addEventListener('DOMContentLoaded', (e) => {
   // при загрузке документа записываем в объект defaultFilters значения фильтров
@@ -27,7 +32,65 @@ function handleUpdateDefault(inputs) {
     handleUpdate(item);
   });
 }
-
+//функция смены пути для картинки
+//отправляем запрос на получение картинки, если картинка не существует - запрашиваем первую картинку
+async function viewNextImage(src) {
+  const img = new Image();
+  const res = await fetch(src);
+  if (res.ok) {
+    img.src = src;
+    img.onload = () => {
+      image.src = src;
+      toggleBtn(btnNext);
+    };
+  } else {
+    if (res.status === 404) {
+      imageCounter = 1;
+      getNextImage(baseLink);
+      toggleBtn(btnNext);
+    }else{
+      console.error(`Response error: ${res.status}`);
+    }
+  }
+}
+//функция получения времени суток(часа)
+function getTimesOfDay() {
+  const data = new Date();
+  const hour = data.getHours();
+  if (hour >= 6 && hour < 12) {
+    return 'morning';
+  }
+  if (hour >= 12 && hour < 18) {
+    return 'day';
+  }
+  if (hour >= 18 && hour < 24) {
+    return 'evening';
+  }
+  if (hour >= 0 && hour < 6) {
+    return 'night';
+  }
+}
+//функция получения след картинки
+function getNextImage(base) {
+  toggleBtn(btnNext);
+  let imgNumber;
+  if (imageCounter >= 0 && imageCounter < 10) {
+    imgNumber = `0${imageCounter}`;
+  } else {
+    imgNumber = imageCounter;
+  }
+  imageCounter++;
+  const fullLink = `${base}${getTimesOfDay()}/${imgNumber}.jpg`;
+  viewNextImage(fullLink);
+}
+//функция переключения активности кнопки
+function toggleBtn(btn) {
+  if (btn.disabled) {
+    btn.disabled = false;
+  } else {
+    btn.disabled = true;
+  }
+}
 filters.addEventListener('input', (event) => handleUpdate(event.target));
-
 btnReset.addEventListener('click', () => handleUpdateDefault(inputs));
+btnNext.addEventListener('click', () => getNextImage(baseLink));
