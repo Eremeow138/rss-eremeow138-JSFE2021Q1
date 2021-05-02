5a2c9c0667c10c664c0791fb8340e385bad5bad6
Aleksei Eremin <eremeow138@yandex.ru>
2021-05-02 12:20:35 +0800

    fix(changeThemes.js): wrap the code in function

--- online-zoo/changeThemes.js
+++ online-zoo/changeThemes.js
@@ -1,16 +1,19 @@
-const checkbox = document.querySelector('.checkbox__input');
-function changeTheme() {
-  if (localStorage.isDark === 'true') {
-    document.documentElement.classList.remove('dark');
-    localStorage.isDark = 'false';
-  } else {
-    document.documentElement.classList.add('dark');
-    localStorage.isDark = 'true';
+function toogleTheme() {
+  const checkbox = document.querySelector('.checkbox__input');
+  function changeTheme() {
+    if (localStorage.isDark === 'true') {
+      document.documentElement.classList.remove('dark');
+      localStorage.isDark = 'false';
+    } else {
+      document.documentElement.classList.add('dark');
+      localStorage.isDark = 'true';
+    }
   }
+  if (localStorage.isDark === 'true') {
+    localStorage.isDark = 'false';
+    checkbox.setAttribute('checked', true);
+    changeTheme();
+  }
+  checkbox.addEventListener('change', changeTheme);
 }
-if (localStorage.isDark === 'true') {
-  localStorage.isDark = 'false';
-  checkbox.setAttribute('checked', true);
-  changeTheme();
-}
-checkbox.addEventListener('change', changeTheme);
+toogleTheme();
