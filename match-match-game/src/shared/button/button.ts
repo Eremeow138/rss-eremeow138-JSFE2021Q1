b10e7be6252889b56c4cc76f44cb4a3b7ccf8fdf
Aleksei Eremin <eremeow138@yandex.ru>
2021-05-20 18:30:03 +0800

    fix: improve button action

--- match-match-game/src/shared/button/button.ts
+++ match-match-game/src/shared/button/button.ts
@@ -1,16 +1,16 @@
 import './button.scss';
 import { BaseComponent } from '../../components/base-component';
+import { Callback } from '../../app.api';
 
 export class Button extends BaseComponent {
-  private text = '';
-
-  constructor(text: string) {
+  constructor(private text = '', private callback: Callback) {
     super('button', ['button']);
     this.text = text;
   }
 
   render(): HTMLElement {
     this.element.innerText = this.text;
+    this.element.addEventListener('click', this.callback);
     return this.element;
   }
 }
