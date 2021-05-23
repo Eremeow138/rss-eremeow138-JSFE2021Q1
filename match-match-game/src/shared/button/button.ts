d53c9f735b93f2967c95ef5554cc81635e307bd7
Aleksei Eremin <eremeow138@yandex.ru>
2021-05-23 12:04:40 +0800

    feat: add type field

--- match-match-game/src/shared/button/button.ts
+++ match-match-game/src/shared/button/button.ts
@@ -3,14 +3,25 @@ import { BaseComponent } from '../../components/base-component';
 import { Callback } from '../../app.api';
 
 export class Button extends BaseComponent {
-  constructor(private text = '', private callback: Callback) {
+  constructor(
+    private text = '',
+    private callback?: Callback,
+    private type?: string,
+  ) {
     super('button', ['button']);
     this.text = text;
   }
 
   render(): HTMLElement {
+    if (this.type) {
+      this.element.setAttribute('type', this.type);
+    }
+
     this.element.innerText = this.text;
-    this.element.addEventListener('click', this.callback);
+    if (this.callback) {
+      this.element.addEventListener('click', this.callback);
+    }
+
     return this.element;
   }
 }
