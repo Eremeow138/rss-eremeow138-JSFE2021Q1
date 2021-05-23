f9e72054a0649c0031f585d11efd82b4274aced9
Aleksei Eremin <eremeow138@yandex.ru>
2021-05-23 12:16:18 +0800

    feat: add type for addRecordCallback and interface for player object

--- match-match-game/src/app.api.ts
+++ match-match-game/src/app.api.ts
@@ -1,7 +1,14 @@
 export type RootElement = HTMLElement | null;
 export type Callback = () => void;
+export type CallbackAddRecord = (str?: string) => Promise<string>;
 export interface ModalService {
   callAll(): void;
 
   subscribeOnModal(callback: () => void): void;
+}
+
+export interface PlayerObject {
+  firstName: string;
+  lastName: string;
+  email: string;
 }
