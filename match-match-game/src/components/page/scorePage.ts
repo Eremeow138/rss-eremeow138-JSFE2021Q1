232171fa4d041b58f8257cd394d47751cfcaea9a
Aleksei Eremin <eremeow138@yandex.ru>
2021-05-24 12:56:56 +0800

    feat: add function of opening registration modal to other pages

--- match-match-game/src/components/page/scorePage.ts
+++ match-match-game/src/components/page/scorePage.ts
@@ -9,6 +9,12 @@ export class ScorePage extends Page {
     this.element.appendChild(new Header(this.modalService).render());
     this.contentField.element.appendChild(new Score().render());
     this.element.appendChild(this.contentField.render());
+
+    this.element.appendChild(this.modal.renderRegistration());
+    this.modalService.cleanCallbacksArray();
+    this.modalService.subscribeOnModal(() => {
+      this.modal.ShowHiddenModal();
+    });
     return this.element;
   }
 }
