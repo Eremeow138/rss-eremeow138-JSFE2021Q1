0c66c93e834aeab3ec98aac6207ffc85bd7cd829
Aleksei Eremin <eremeow138@yandex.ru>
2021-05-24 12:18:04 +0800

    feat: add highlighting of active page in navigation

--- match-match-game/src/components/header/header-item/nav/nav-item/nav-item.ts
+++ match-match-game/src/components/header/header-item/nav/nav-item/nav-item.ts
@@ -9,6 +9,8 @@ export class NavItem extends BaseComponent {
 
   private link: string;
 
+  private hash: string;
+
   constructor(
     link: string,
     icon: string,
@@ -16,12 +18,18 @@ export class NavItem extends BaseComponent {
     extraIconClasses?: string[],
   ) {
     super('a', ['nav__item']);
+
     if (extraIconClasses) {
       this.extraIconClasses = extraIconClasses.join(' ');
     }
     this.icon = icon;
     this.text = text;
     this.link = link;
+    this.hash = window.location.hash.slice(1).toLowerCase() || '/';
+    if (this.hash === (this.link.slice(1) || '/')) {
+      this.element.classList.add('nav__item_active');
+    }
+
     this.element.setAttribute('href', this.link);
   }
 
