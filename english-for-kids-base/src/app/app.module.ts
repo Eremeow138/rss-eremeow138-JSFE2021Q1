5f480f4748c9b38ee301de9a4583ab4a48d1c0f0
Aleksei Eremin <eremeow138@yandex.ru>
2021-06-28 15:12:52 +0800

    feat: implement opening and closing the navigation

--- english-for-kids-base/src/app/app.module.ts
+++ english-for-kids-base/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { ClickOutsideModule } from 'ng-click-outside';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CardDataService, GameService, InMemoryDataService } from './services';
@@ -29,6 +30,7 @@ import {
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
       dataEncapsulation: false,
     }),
+    ClickOutsideModule,
   ],
   providers: [CardDataService, GameService],
   bootstrap: [AppComponent],
--- english-for-kids-base/src/app/components/navigation/navigation.component.ts
+++ english-for-kids-base/src/app/components/navigation/navigation.component.ts
@@ -27,4 +27,10 @@ export class NavigationComponent implements OnInit {
   toggleMenu(): void {
     this.isOpen = !this.isOpen;
   }
+
+  closeMenu(): void {
+    if (this.isOpen) {
+      this.toggleMenu();
+    }
+  }
 }
